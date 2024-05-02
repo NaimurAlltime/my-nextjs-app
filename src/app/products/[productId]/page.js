921e1dcd701c72cf2a5452537b135def040eaa8d
Naimur Rahman <naimur2231@gmail.com>
2024-05-02 02:00:08 -0700

    Exploring Dynamic Routes

---
+++ src/app/products/[productId]/page.js
@@ -0,0 +1,6 @@
+const DynamicProduct = ({ params, searchParams }) => {
+  // console.log({ searchParams });
+  return <div>DynamicProduct {params.productId}</div>;
+};
+
+export default DynamicProduct;
