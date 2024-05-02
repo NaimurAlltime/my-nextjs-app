f8c94abf52d071c87469a52a9773df6badd3f49f
Naimur Rahman <naimur2231@gmail.com>
2024-05-02 05:35:56 -0700

    create loading and notfound page

---
+++ src/app/loading.js
@@ -0,0 +1,5 @@
+const LoadingPage = () => {
+  return <div className="text-4xl text-center text-sky-500">Loading...</div>;
+};
+
+export default LoadingPage;
---
+++ src/app/not-found.js
@@ -0,0 +1,7 @@
+const NotFoundPage = () => {
+  return (
+    <div className="text-4xl text-center text-gray-700"> 404 Not Found </div>
+  );
+};
+
+export default NotFoundPage;
