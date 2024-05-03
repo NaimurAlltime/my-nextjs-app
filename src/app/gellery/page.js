4ba5a45c0c7636f0049bd4311b79b6cc01fc5407
Naimur Rahman <naimur2231@gmail.com>
2024-05-03 02:14:50 -0700

    explore Import Alias

--- src/app/gellery/page.js
+++ src/app/gellery/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import nextJSImage from "../../assets/next-js-img.jpg";
+import nextJSImage from "@/assets/next-js-img.jpg";
 
 export const metadata = {
   title: "Gellery",
